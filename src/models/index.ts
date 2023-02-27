1308419d8bd66e66e4c07fa164bb5d5e0adb4972
NNICEEt <niceteerapat@gmail.com>
2023-02-27 14:30:08 +0700

    add model and modify hooks

---
+++ src/models/index.ts
@@ -0,0 +1,16 @@
+export interface DistrictPopulationGrowth {
+  "2550": string;
+  "2551": string;
+  "2552": string;
+  "2553": string;
+  "2554": string;
+  "2555": string;
+  "2556": string;
+  "2557": string;
+  "2558": string;
+  "2559": string;
+  dcode: number;
+  name: string;
+}
+
+export type BangkokPopulationGrowth = DistrictPopulationGrowth[];
