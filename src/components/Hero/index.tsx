9c71a549ecfb72fb0515bda44deba8c9d5c5923b
NNICEEt <niceteerapat@gmail.com>
2023-03-07 16:09:45 +0700

    change file naming

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import * as constants from "./constants";
+import * as locales from "./locales";
 
 const Hero = () => {
   console.log("hero component");
   return (
     <div>
-      <div className="text-28 font-bold text-white">{constants.TITLE}</div>
-      <div className="pt-26px text-18 font-bold">{constants.SUBTITLE}</div>
+      <div className="text-28 font-bold text-white">{locales.TITLE}</div>
+      <div className="pt-26px text-18 font-bold">{locales.SUBTITLE}</div>
       <div className="pt-12px whitespace-pre-line text-14">
-        {constants.CONTENT}
+        {locales.CONTENT}
       </div>
     </div>
   );
